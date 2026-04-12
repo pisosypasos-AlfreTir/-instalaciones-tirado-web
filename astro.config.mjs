52e28a63a293bee11c8b427505c8ce86308b877a
pisosypasos-AlfreTir <pisosypasos@gmail.com>
2026-04-12 17:04:37 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.instalacionestirado.com',
+    integrations: [
+        tailwind(),
+        sitemap(),
+    ],
+});
